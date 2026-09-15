a15b2a7c7467bb10a68b29e9f3fba178d832057e
Knightmare <114373431+KnightmareVIIVIIXC@users.noreply.github.com>
2026-09-15 18:51:21 -0400

    Update create_oisd_excludes.py

--- exclusions/create_oisd_excludes.py
+++ exclusions/create_oisd_excludes.py
@@ -10,8 +10,7 @@ for line in soup.get_text("\n").splitlines():
     line = line.strip().lower()
     if not line:
         continue
-    if " " not in line and "." in line:
-        domains.append(line)
+    domains.append(line)
 domains = sorted(set(domains))
 with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
     f.write("\n".join(domains))
